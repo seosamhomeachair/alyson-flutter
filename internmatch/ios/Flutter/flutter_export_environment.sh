583f44961972a98b851045d8fc83c6776fe6c537
BARAD GHIMIRE <ghimireb@deakin.edu.au>
2019-12-31 10:41:30 +1100

    migrated to androidX, updated .gitignore, and other work

---
+++ internmatch/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/barad/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/barad/projects/genny/alyson-flutter/internmatch"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/barad/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
