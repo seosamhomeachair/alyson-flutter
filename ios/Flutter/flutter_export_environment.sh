550cec3d047877ff84f09c313e64f75e0b9b3b49
BARAD GHIMIRE <ghimireb@deakin.edu.au>
2020-01-03 10:42:27 +1100

    updated projectEnv.dart, also enabled web support

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/barad/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/barad/projects/genny/alyson-flutter/internmatch"
+export "FLUTTER_APPLICATION_PATH=/Users/barad/projects/genny/alyson_flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- lib/projectEnv.dart
+++ lib/projectEnv.dart
@@ -1,6 +1,6 @@
 class Project{
-  static var url = "https://app3-internmatch-staging.outcome-hub.com";
-  static var projectColor = 0xFF442B2D;
+  static var url = "https://internmatch.outcome-hub.com";
+  static var projectColor = 0xFF283A4C;
   static var projectName ="InternMatch";
   var fontsize =3;
   static var img = "images/internmatch_logo.png";
